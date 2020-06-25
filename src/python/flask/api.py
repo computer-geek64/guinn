ad3c27eb8b3514483d0bc1cb590bd99108651983
Ashish D'Souza <sudouser512@gmail.com>
2020-06-25 00:26:48 -0400

    Add TensorFlow v2 generator code

--- src/python/flask/api.py
+++ src/python/flask/api.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # api.py
 
+import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import os
 from config import FLASK_IP, FLASK_PORT
 from flask import Flask, jsonify, redirect, request, render_template
 
