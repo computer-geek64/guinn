eeca4c056a4add9bb446873b8f8207023a54b2a7
Ashish D'Souza <sudouser512@gmail.com>
2020-06-24 22:38:42 -0400

    Add Flask config entries

--- src/python/config.py
+++ src/python/config.py
@@ -4,3 +4,7 @@
 if __name__ == '__main__':
     exit(0)
 
+
+FLASK_IP = '0.0.0.0'  # IP address for Flask API server (use '0.0.0.0' to allow inbound connections from other computers)
+FLASK_PORT = 81  # Port for Flask API server
+
--- src/python/flask/api.py
+++ src/python/flask/api.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 # api.py
 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import os
+from config import FLASK_IP, FLASK_PORT
 from flask import Flask, jsonify, redirect, request, render_template
 
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
 
+
+pass
+
+
+if __name__ == "__main__":
+    app.run(app.config["IP"], app.config["PORT"])
