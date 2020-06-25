8ee29bdd7f58e04d490b4d032a28b3d5f383ef95
Ashish D'Souza <sudouser512@gmail.com>
2020-06-25 18:24:18 -0400

    Add error templates

--- src/python/flask/api.py
+++ src/python/flask/api.py
@@ -16,5 +16,27 @@ def get_home():
     return 'Hello world!'
 
 
+# Error handlers
+@app.errorhandler(404)
+def error_404(e):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(400)
+def error_400(e):
+    return "HTTP 400 - Bad Request", 400
+
+
+@app.errorhandler(500)
+def error_500(e):
+    return render_template("errors/500.html"), 500
+
+
+@app.errorhandler(403)
+def error_403(e):
+    return render_template("errors/403.html"), 403
+
+
 if __name__ == "__main__":
     app.run(FLASK_IP, FLASK_PORT)
+
