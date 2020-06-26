c6441503a5fd5a7a7572e2cb46a6e71c1b2f25d8
Ashish D'Souza <sudouser512@gmail.com>
2020-06-26 15:54:08 -0400

    Add session variables to Flask

--- src/python/config.py
+++ src/python/config.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
 
 FLASK_IP = '0.0.0.0'  # IP address for Flask API server (use '0.0.0.0' to allow inbound connections from other computers)
 FLASK_PORT = 80  # Port for Flask API server
+FLASK_SECRET_KEY = ''  # Secret key for Flask variables
 
 DEBUG_TOKEN = ''  # Authorization token for remote debugging
 
--- src/python/flask/api.py
+++ src/python/flask/api.py
@@ -5,16 +5,31 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import git
-from config import FLASK_IP, FLASK_PORT, DEBUG_TOKEN
-from flask import Flask, jsonify, redirect, request, render_template
+import json
+import shutil
+from uuid import uuid4
+from config import FLASK_IP, FLASK_PORT, FLASK_SECRET_KEY, DEBUG_TOKEN
+from flask import Flask, jsonify, redirect, session, request, render_template
 
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = FLASK_SECRET_KEY
 
 
+# Additional functions
+def delete_orphaned_networks():
+    networks = next(os.walk(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'networks')))[1]
+    for network in networks:
+        if network not in session:
+            shutil.rmtree(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'networks', network))
+
+
+# Endpoints
 @app.route('/', methods=['GET'])
 def get_home():
     return 'Hello world!', 200
+
 
 @app.route('/git/pull/', methods=['GET'])
 def get_git_pull():
@@ -22,6 +37,33 @@ def get_git_pull():
         git.cmd.Git(os.path.dirname(os.path.abspath(__file__))).pull()
         return 'Pulling', 200
     return error_403(403)
+
+
+@app.route('/networks/create/', methods=['POST'])
+def get_networks_create():
+    delete_orphaned_networks()
+
+    if 'layers' not in request.form:
+        return error_400(400)
+
+    if 'nn' not in session:
+        uuid = str(uuid4())
+        while uuid in session.values():
+            uuid = str(uuid4())
+        session['nn'] = uuid
+
+    os.makedirs(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'networks', session['nn']))
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'networks', session['nn'], 'network.json'), 'w') as file:
+        file.write(request.form.get('layers'))
+    return 'Success', 200
+
+
+@app.route('/networks/delete/', methods=['GET'])
+def get_networks_delete():
+    if 'nn' in session:
+        session.pop('nn')
+    delete_orphaned_networks()
+    return 'Success', 200
 
 
 # Error handlers
@@ -47,4 +89,3 @@ def error_403(e):
 
 if __name__ == '__main__':
     app.run(FLASK_IP, FLASK_PORT)
-
