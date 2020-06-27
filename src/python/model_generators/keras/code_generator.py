e64eb25d3afeba6e79db51111234ee3a8d9205aa
Varun Lakshmanan <varunlakshmanan@outlook.com>
2020-06-27 02:00:11 -0400

    Added add_layers function

--- src/python/model_generators/keras/code_generator.py
+++ src/python/model_generators/keras/code_generator.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 # code_generator.py
 
-def generate_code():
+import tensorflow as tf
+
+template = '''model = tf.keras.Sequential()'''
+
+def add_layers(layers):
+    for layer in layers:
+        if layer['type'] == 'dense':
+            template += '''model.add(tf.keras.layers.Dense(units=, activation=None))'''  # add the actual variables
+        elif layer['type'] == 'flatten':
+            template += '''model.add(tf.keras.layers.Flatten())'''
+        elif layer['dropout'] == 'dropout':
+            
+
+def generate_template(name, layers, x_training_data=None, y_training_data=None, loss=None, optimizer=None,
+                  learning_rate=None, epochs=None, batch_size=None):
     # generate keras code
-    return code
