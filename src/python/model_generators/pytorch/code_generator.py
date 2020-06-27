a3ea6c564fdda043b5e3608f315c56f8ac1608d1
Pranav Pusarla <ppusarla@gmail.com>
2020-06-27 14:16:30 -0700

    modified function for generating pytorch code

--- src/python/model_generators/pytorch/code_generator.py
+++ src/python/model_generators/pytorch/code_generator.py
@@ -10,22 +10,22 @@ def generate_code(layers):
     for i in range(len(layers)):
         if (layers[i]['name'] == 'dense'):
             code_template += 'layers_list.append(nn.Linear({previous_layer_nodes}, {layer_nodes}))'\
-                .format(previous_layer_nodes=layers[i]['nodes'], layer_nodes=layers[i]['nodes'])
+                .format(previous_layer_nodes=layers[i-1]['nodes'], layer_nodes=layers[i]['nodes'])
         elif (layers[i]['name'] == 'flatten'):
             code_template += 'layers_list.append(nn.Flatten())'
         elif (layers[i]['name'] == 'dropout'):
             code_template += 'layers_list.append(nn.Dropout2d(p={probability}))'\
-                .format(probability=layers[i]['probability'])
+                .format(probability=layers[i]['rate'])
         elif (layers[i]['name'] == 'batch_normalization'):
-            code_template += 'layers_list.append(nn.BatchNorm1d(num_features={num_of_features}))'\
-                .format(num_of_features=layers[i]['num_of_features'])
+            code_template += 'layers_list.append(nn.BatchNorm1d(num_features={num_of_features}, eps={eps}, momentum={momentum}))'\
+                .format(num_of_features=layers[i]['num_of_features'], eps=layers['i']['epsilon'], momentum=layers['i']['momentum'])
         elif(layers[i]['name'] == 'conv_2d'):
-            code_template += 'layers_list.append(nn.Conv2d({input_channels}, {output_channels}, {kernel_size}, stride={stride}, padding={padding}))'\
+            code_template += 'layers_list.append(nn.Conv2d({input_channels}, {output_channels}, {kernel_size}, stride={stride}))'\
                 .format(input_channels=layers[i]['input'], output_channels=layers[i]['output'], kernel_size=layers[i]['kernel_size'],
-                        stride = layers[i]['stride'], padding= layers[i]['padding'])
+                        stride = layers[i]['stride'])
         elif(layers[i]['name'] == 'max_pooling_2d'):
-            code_template += 'layers_list.append(nn.MaxPool2d(kernel_size = {kernel_size}))'\
-                .format(kernel_size = layers[i]['kernel_size'])
+            code_template += 'layers_list.append(nn.MaxPool2d(kernel_size = {kernel_size}, stride={stride}))'\
+                .format(kernel_size = layers[i]['kernel_size'], stride=layers[i]['stride'])
         elif (layers[i]['name'] == 'average_pooling_2d'):
             code_template += 'layers_list.append(nn.AvgPool2d(kernel_size = {kernel_size}))' \
                 .format(kernel_size=layers[i]['kernel_size'])
